1cb6b7bb12f09f283af92c82d6a2371431b170e3
Joaquin <joaquin.solla@udc.es>
2022-03-04 11:16:35 +0100

    Working on main.py

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+import tweepy
+import json
+import random
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from Twitter_Files.tw_credentials import *
 
+# Abrimos el archivo json
+with open('word_database.json') as f:
+    data = json.load(f)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Nos autenticamos en Twitter
+auth = tweepy.OAuthHandler(botUsername, botPassword)
+auth.set_access_token(apiKey, apiKeySecret)
 
+# Creamos un objeto de la libreria
+api = tweepy.API(auth)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Se calcula la palabra del dia
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#CALCULAR
+
+# borrar:
+today_index = 0
+while data[today_index]['appear'] == "True":
+    today_index = int(random.uniform(0, len(data)))
+    if data[today_index]['appear'] == "False":
+        data[today_index]['appear'] = "True"
+        break
+
+# Abrimos el archivo json con permisos para escritura
+with open('word_database.json', 'w') as f:
+    f.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+
+# Guardamos el mensaje final
+msg = f"¡El Encasillado de hoy ha sido {data[today_index]['word']}!"
+
+# Tuiteamos el resultado final pasándole el parámetro de mensaje y la imagen.
+api.update_status(msg)
