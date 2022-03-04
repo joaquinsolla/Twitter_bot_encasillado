c5ccc3d3dc0653ebb76cf8c73a915a76c024cf2f
Joaquin <joaquin.solla@udc.es>
2022-03-04 14:30:24 +0100

    Working on main.py

--- main.py
+++ main.py
@@ -8,28 +8,27 @@ from Twitter_Files.tw_credentials import *
 with open('word_database.json') as f:
     data = json.load(f)
 
-# Nos autenticamos en Twitter
-auth = tweepy.OAuthHandler(botUsername, botPassword)
-auth.set_access_token(apiKey, apiKeySecret)
-
-# Creamos un objeto de la libreria
-api = tweepy.API(auth)
+# Nos autenticamos en Twitter e iniciamos el cliente
+client = tweepy.Client(consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_token, access_token_secret=access_token_secret)
 
 # Se calcula la palabra del dia
 json_len = len(data)
-print("json_len = " + json_len)
+print("json_len = " + str(json_len))
 
+
+#int(datetime.date.day) * int(datetime.date.month) * int(datetime.date.year)
 todayIndex = 0
-todayIndex = round((3.14159265359 * datetime.date.day * datetime.date.month * datetime.date.year * 1000) % json_len) - 1
-print("today_index = " + todayIndex)
+todayIndex = round((3.14159265359 * 2 * 1000) % json_len) - 1
+print("today_index = " + str(todayIndex))
 
 wotd = "N/A"
-wotd = {data[todayIndex]['word']}
-print("wotd = " + wotd)
+wotd = data[todayIndex]['word']
+print("wotd = " + str(wotd))
 
 # Guardamos el mensaje final
-msg = f"¡El Encasillado de hoy ha sido " + wotd + "!"
+msg = f"El #Encasillado de hoy ha sido: " + str(wotd) + "\n\nhttps://play.google.com/store/apps/details?id=com.joa.encasillado"
 print(msg)
 
-# Tuiteamos el resultado final pasándole el parámetro de mensaje y la imagen.
-api.update_status(msg)
+# Publicamos el tweet
+client.create_tweet(text=msg)
+print("\nTWEET CREATED AND PUBLISHED")
