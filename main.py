c2854724a4f38556befc1e4e97b5be4e6d5d6692
Joaquin <joaquin.solla@udc.es>
2022-04-18 23:55:30 +0200

    Bot adapted to Encasillado v1.2.3 (released).

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import datetime
 
 from tw_credentials import *
 
-# VERSION DE APP (Next: 1.2.3)
-appVersion = "1.2.1"
+# VERSION DE APP (Next: -)
+appVersion = "1.2.3"
 print("Encasillado Bot - App-version: " + appVersion)
 
 # Abrimos el archivo json
